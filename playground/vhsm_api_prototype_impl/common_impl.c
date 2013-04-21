a8492f3b694f4e973d316e1fbd6152f3d6fedb3a
Sergey A. Savenko <sergey.a.savenko@gmail.com>
2013-04-21 17:57:39 +0400

    added common.h implementation

--- playground/vhsm_api_prototype_impl/common_impl.c
+++ playground/vhsm_api_prototype_impl/common_impl.c
@@ -1,5 +1,7 @@
 #include <vhsm_api_prototype/common.h>
 
+#include "transport.h"
+
 // Start a new session. Session structure pointed to by session_ptr is initialized if this call succeeds.
 // Can return: VHSM_RV_OK, VHSM_RV_BAD_SESSION, VHSM_RV_BAD_ARGUMENTS
 vhsm_rv vhsm_start_session(vhsm_session * session_ptr) {
@@ -7,28 +9,23 @@ vhsm_rv vhsm_start_session(vhsm_session * session_ptr) {
     return VHSM_RV_BAD_ARGUMENTS;
   }
   
-  //TODO call some transport function
-  
-  return VHSM_RV_OK;
+  return vhsm_tr_start_session(session_ptr);
 }
 
 // End session.
 // Can return: VHSM_RV_OK, VHSM_RV_BAD_SESSION
 vhsm_rv vhsm_end_session(vhsm_session session) {
-  //TODO call some transport function
-  return VHSM_RV_OK;
+  return vhsm_tr_end_session(session);
 }
 
 // Attempt to login.
 // Can return: VHSM_RV_OK, VHSM_RV_BAD_SESSION, VHSM_RV_BAD_CREDENTIALS
 vhsm_rv vhsm_login(vhsm_session session, vhsm_credentials credentials) {
-  //TODO call some transport function
-  return VHSM_RV_OK;
+  return vhsm_tr_login(session, credentials);
 }
 
 // Logout
 // Can return: VHSM_RV_OK, VHSM_RV_BAD_SESSION
 vhsm_rv vhsm_logout(vhsm_session session) {
-  //TODO call some transport function
-  return VHSM_RV_OK;
+  return vhsm_tr_logout(session);
 }
