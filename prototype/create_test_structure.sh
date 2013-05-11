e2f3c8f3623bb2d454b625c444ed6d7c39851f95
Sergey A. Savenko <sergey.a.savenko@gmail.com>
2013-05-11 17:34:29 +0400

    added a script for test structure creation

---
+++ prototype/create_test_structure.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+TEST_DIR="test_drive"
+CLIENT_DIR=$TEST_DIR"/1"
+
+IO_VHSM_TO_HOST=$TEST_DIR"/io_vhsm_to_host"
+IO_VHSM_FROM_HOST=$TEST_DIR"/io_vhsm_from_host"
+
+IO_CLIENT_TO_HOST=$CLIENT_DIR"/send_data"
+IO_CLIENT_FROM_HOST=$CLIENT_DIR"/recv_data"
+
+
+
+echo -e "RUNNING MAKE CLEAN\n"
+
+make clean
+
+echo -e "RUNNING MAKE\n"
+
+make
+
+
+
+if [[ -e $TEST_DIR ]]; then
+  echo -e "$TEST_DIR/ ALREADY EXISTS. REMOVING.\n"
+  rm -rf $TEST_DIR
+fi;
+
+echo -e "CREATING FILES AND DIRECTORIES\n"
+
+mkdir $TEST_DIR
+mkdir $CLIENT_DIR
+
+touch $IO_VHSM_FROM_HOST
+touch $IO_VHSM_TO_HOST
+touch $IO_CLIENT_FROM_HOST
+touch $IO_CLIENT_TO_HOST
+
+echo -e "COPYING BINARIES\n"
+
+cp "client/test_app/test_app" $CLIENT_DIR
+cp "vhsm/vhsm" $TEST_DIR
+cp "host/host" $TEST_DIR
