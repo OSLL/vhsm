9f0dfb94a3a9f90dd6349af19343197351f804d6
Sergey A. Savenko <sergey.a.savenko@gmail.com>
2013-05-18 18:56:17 +0400

    added store_object method to Namespace;

--- prototype/vhsm/esapi/Namespace.h
+++ prototype/vhsm/esapi/Namespace.h
@@ -15,6 +15,7 @@ namespace ES {
     virtual bool contains_object(std::string const & name) const = 0;
     virtual SecretObject load_object(std::string const & name) const = 0;
     
+    virtual bool store_object(std::string const & name, void const * data, size_t size);
     virtual bool delete_object(std::string const & name) = 0;
     
     virtual ~Namespace() {
