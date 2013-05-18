4d3f16b58e91690c2475b827cda04797e610a44e
Sergey A. Savenko <sergey.a.savenko@gmail.com>
2013-05-18 20:38:45 +0400

    implemented list_directories and list_files;

--- prototype/vhsm/esapi_file_impl/FsUtil.h
+++ prototype/vhsm/esapi_file_impl/FsUtil.h
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <dirent.h>
 
 
 namespace ES {
@@ -36,16 +37,39 @@ namespace ES {
     }
     
     static std::vector<std::string> list_directories(std::string const & path) {
-      //TODO implement me;
-      return std::vector<std::string>();
+      std::vector<std::string> names;
+      
+      file_names(path, names, DT_DIR);
+      
+      return names;
     }
     
     static std::vector<std::string> list_files(std::string const & path) {
-      //TODO implement me;
-      return std::vector<std::string>();
+      std::vector<std::string> names;
+      
+      file_names(path, names, DT_REG);
+      
+      return names;
     }
     
   private:
+    static void file_names(std::string const & path, std::vector<std::string> & names, unsigned char file_type) {
+      DIR *dir = opendir(path.c_str());
+      struct dirent *de = 0;
+      
+      if (0 == dir) {
+        return;
+      }
+      
+      while (0 != (de = readdir(dir))) {
+        if (file_type == de->d_type) {
+          names.push_back(std::string(de->d_name));
+        }
+      }
+      
+      closedir(dir);
+    }
+    
     static bool read_stat(std::string const & path, struct stat * s) {
       return 0 == stat(path.c_str(), s);
     }
