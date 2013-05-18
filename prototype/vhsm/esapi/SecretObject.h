833230ec8f5c686764a934809d223db835e33c03
Sergey A. Savenko <sergey.a.savenko@gmail.com>
2013-05-18 18:28:22 +0400

    implemented SecretObject;

--- prototype/vhsm/esapi/SecretObject.h
+++ prototype/vhsm/esapi/SecretObject.h
@@ -5,31 +5,64 @@
 namespace ES {
 
   class SecretObject {
+  private:
+    struct SODataHolder {
+      std::string my_name;
+      size_t my_size;
+      void * my_bytes;
+      int my_ref_count;
+      
+      SODataHolder(std::string const & name, void * data, size_t size)
+        : my_name(name), my_size(size), my_bytes(data), my_ref_count(1) {
+      }
+      
+      void hold() {
+        ++my_ref_count;
+      }
+      
+      void release() {
+        if (0 == --my_ref_count) {
+          delete this;
+        }
+      }
+      
+      ~SODataHolder() {
+          delete [] my_bytes;
+      }
+    };
   public:
-    SecretObject(std::string const & name, void * data, size_t size) :
-      my_name(name), my_bytes(data), my_size(size), ref_count(0) {
-      //TODO implement me
+    SecretObject(std::string const & name, void * data, size_t size)
+      : my_hldr(new SODataHolder(name, data, size)){
     }
     
-    //TODO add copy constructor, assignment operator.
+    SecretObject(SecretObject const & so) {
+      my_hldr = so.my_hldr;
+      my_hldr->hold();
+    }
+    
+    SecretObject & operator=(SecretObject const & so) {
+      my_hldr->release();
+      my_hldr = so.my_hldr;
+    }
     
     void const * raw_bytes() {
-      return my_bytes;
+      return my_hldr->my_bytes;
     }
     
     size_t size() {
-      return my_size;
+      return my_hldr->my_size;
     }
     
     std::string const & name() {
-      return my_name;
+      return my_hldr->my_name;
+    }
+    
+    ~SecretObject() {
+      my_hldr->release();
     }
     
   private:
-    std::string my_name;
-    size_t my_size;
-    void * my_bytes;
-    int * ref_count;
+    SODataHolder * my_hldr;
   };
 
 }
