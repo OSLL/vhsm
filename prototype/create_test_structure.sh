81268cece81e2d89092f00bc9822347b9b2ba701
root <root@debian.(none)>
2013-07-12 01:34:38 +0400

    extended create_test_structure.sh script to run tests

--- prototype/create_test_structure.sh
+++ prototype/create_test_structure.sh
@@ -2,14 +2,7 @@
 
 TEST_DIR="test_drive"
 CLIENT_DIR=$TEST_DIR"/1"
-
-IO_VHSM_TO_HOST=$TEST_DIR"/io_vhsm_to_host"
-IO_VHSM_FROM_HOST=$TEST_DIR"/io_vhsm_from_host"
-
-IO_CLIENT_TO_HOST=$CLIENT_DIR"/send_data"
-IO_CLIENT_FROM_HOST=$CLIENT_DIR"/recv_data"
-
-
+TESTS=$PWD"/client/test_app"
 
 echo -e "RUNNING MAKE CLEAN\n"
 
@@ -18,7 +11,9 @@ make clean
 echo -e "RUNNING MAKE\n"
 
 make
-
+cd ./netlink_transport/kernel/
+make
+cd ../../
 
 
 if [[ -e $TEST_DIR ]]; then
@@ -31,23 +26,29 @@ echo -e "CREATING FILES AND DIRECTORIES\n"
 mkdir $TEST_DIR
 mkdir $CLIENT_DIR
 
-touch $IO_VHSM_FROM_HOST
-touch $IO_VHSM_TO_HOST
-touch $IO_CLIENT_FROM_HOST
-touch $IO_CLIENT_TO_HOST
 
 echo -e "COPYING BINARIES\n"
 
-cp "client/test_app/test_app" $CLIENT_DIR
+#cp "client/test_app/test_app" $CLIENT_DIR
 cp "vhsm/vhsm" $TEST_DIR
-cp "host/host" $TEST_DIR
+#cp "host/host" $TEST_DIR
 cp "vhsm/vhsm_admin" $TEST_DIR
 cp "client/openssl_vhsm_engine/test_engine.so" $CLIENT_DIR
 cp "client/openssl_vhsm_engine/e_test_app" $CLIENT_DIR
 
+echo -e "INIT NETLINK MODULE"
+insmod ./netlink_transport/kernel/vhsm_transport.ko
+
+echo -e "COPY VHSM"
+./copy_vhsm.sh
 
 echo -e "INITILIZING ENCRYPTED STORAGE\n"
 
-cd $TEST_DIR
-./vhsm_admin i ./data
-./vhsm_admin c ./data user password
+cd /var/lib/vz/private/411/ 
+./vhsm &
+
+echo -e "PAUSE"
+sleep 5
+echo -e "RUN TESTS"
+cd $TESTS
+./run_tests.sh
